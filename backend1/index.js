0997bcd639666662f025838a668136cae89a1c1d
Ritheeshshetty <ritheeshshetty77@gmail.com>
2025-05-21 12:14:19 +0530

    changed code

--- backend1/index.js
+++ backend1/index.js
@@ -214,7 +214,7 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     process.env.FRONTEND_URL || "http://localhost:3000",
-    // "https://sunshine-frontend-sable.vercel.app/",
+    
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
